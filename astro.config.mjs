783d66c1fb04fc4c87851240521d5c277c95cdcb
Mikiyas Tibebu <mikitebe123@gmail.com>
2024-08-06 01:53:09 +0300

    add missing dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://miki-tebe.vercel.app",
-  integrations: [react(), markdoc(), keystatic(), tailwind()],
+  integrations: [react(), markdoc(), keystatic(), tailwind(), sitemap()],
   output: "hybrid",
   adapter: vercel()
 });
